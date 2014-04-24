24469dc825f8ea1afee0d9d580c82bf1a44458ae
msimonin <matthieu.simonin@inria.fr>
2014-04-24 21:28:52 +0200

    initial.
    M

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+load "config.rb"
+load "config/deploy/xp5k.rb"
+load "config/lib/spinner.rb"
+
+
+# relative to the capfile
+set :recipes_path, "./node_modules"
+
+# Enable pretty output. Remove it if you want full logging
+#logger.level = Logger::IMPORTANT
+#STDOUT.sync
+
+
+
+# load recipes
+recipes = Dir.glob("#{recipes_path}/*");
+recipes.each do |recipe|
+    load "#{recipe}/recipe.rb"
+end
+
+#override default roles
+#ad specs roles.
+roles = Dir.glob("specs/*/roles.rb")
+roles.each do |role|
+    load "#{role}"
+end
+
+
+if File.exist?("roles.rb")
+  load "roles.rb"
+end
+
